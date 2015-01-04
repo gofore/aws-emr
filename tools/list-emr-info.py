2d602beffeb0d2869b9d224319d9fb8a25782dc8
Ville Seppanen <ville.seppanen@gofore.com>
2015-01-04 22:57:59 +0200

    Get more data

--- tools/list-emr-info.py
+++ tools/list-emr-info.py
@@ -9,6 +9,33 @@ import pprint
 
 conn = boto.emr.connect_to_region('eu-west-1')
 
-for cluster in conn.list_clusters().clusters:
-    pprint.pprint(vars(cluster))
-    pprint.pprint(vars(cluster.status))
+def get_clusters():
+    for cluster in conn.list_clusters().clusters:
+        
+        cluster_info = cluster.__dict__
+        cluster_info['status'] = cluster.status.__dict__
+        cluster_info['status']['timeline'] = cluster.status['timeline'].__dict__
+        
+        instance_groups_infos = conn.list_instance_groups(cluster.id).instancegroups
+        instances = []
+        for instance_group in instance_groups_infos:
+            instances.append(instance_group.__dict__)
+
+        cluster_info['instances'] = instances
+        
+        pprint.pprint(cluster_info, indent=4)
+        print "\n"
+
+def get_jobflows():
+    for jobflow in conn.describe_jobflows():
+        pprint.pprint(vars(jobflow))
+        try:
+            if jobflow.instancegroups:
+                for instancegroup in jobflow.instancegroups:
+                    pprint.pprint(vars(instancegroup), indent=8)
+        except:
+            pass
+        print "\n"
+
+if __name__ == "__main__":
+    get_clusters()

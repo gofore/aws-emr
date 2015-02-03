8a01dad545b0473d538d62874c417136ef4195ec
Ville Seppanen <ville.seppanen@gofore.com>
2015-02-03 19:43:03 +0200

    Simplified count cars

--- src/streaming-programs/count-cars.py
+++ src/streaming-programs/count-cars.py
@@ -2,11 +2,7 @@
 import sys
 import json
 
-def main():
-    for input_line in sys.stdin:
-        data = json.loads(input_line)
-        for recognition in data['recognitions']:
-            print "LongValueSum:" + str(recognition['id']) + "\t" + str(recognition['cars'])
-
-if __name__ == "__main__":
-    main()
+for input_line in sys.stdin:
+    data = json.loads(input_line)
+    for recognition in data['recognitions']:
+        print "LongValueSum:" + str(recognition['id']) + "\t" + str(recognition['cars'])
